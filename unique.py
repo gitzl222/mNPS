adea54773c86f86dae32408327e907c2006115a9
gitzl222 <90139726+gitzl222@users.noreply.github.com>
2022-03-02 11:04:30 +0800

    Update unique.py

--- unique.py
+++ unique.py
@@ -7,6 +7,7 @@ result=sys.argv[2]
 list=[]
 outfile=open(result,'w')
 Infile=open(filter,'r').readlines()
+#To filter out the best match which have a highest reads identity of unique Subject id and output a XX.result.txt.
 for line in Infile:
 	linelist=line[:-1].split('\t')
 	geneid='\t'.join(linelist[0:2])
